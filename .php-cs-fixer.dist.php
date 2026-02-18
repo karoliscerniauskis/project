9e277e954b9adf7c7fe0e4d025992ec435f99292
Karolis Černiauskis <kcerniauskis@gmail.com>
2026-02-19 00:01:00 +0200

    Update PHP CS Fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,27 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
+$finder = new PhpCsFixer\Finder()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->filter(function (\SplFileInfo $file) {
+        $realPath = $file->getRealPath();
+        if ($realPath === false) {
+            return true;
+        }
+
+        return $realPath !== realpath(__DIR__ . '/src/Kernel.php');
+    })
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        // Ensure every file ends with a newline
+        'single_blank_line_at_eof' => true,
+        // Add: declare(strict_types=1); at the top of PHP files (where applicable)
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder)
 ;
