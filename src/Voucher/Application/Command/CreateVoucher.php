285cc0c1a1d6ec44baeb809d9194f0e7a8f71912
Karolis Černiauskis <kcerniauskis@gmail.com>
2026-02-22 21:17:25 +0200

    Add readonly keyword to commands

--- src/Voucher/Application/Command/CreateVoucher.php
+++ src/Voucher/Application/Command/CreateVoucher.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace App\Voucher\Application\Command;
 
-final class CreateVoucher
+final readonly class CreateVoucher
 {
 }
--- src/Voucher/Application/Command/DeactivateVoucher.php
+++ src/Voucher/Application/Command/DeactivateVoucher.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace App\Voucher\Application\Command;
 
-final class DeactivateVoucher
+final readonly class DeactivateVoucher
 {
 }
