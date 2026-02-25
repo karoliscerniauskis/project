acb5f5d7688bc132ebe6fe2f8313cf7cf5446f92
Karolis Černiauskis <kcerniauskis@gmail.com>
2026-02-25 21:30:30 +0200

    Refactor Voucher to extend AbstractAggregateRoot

--- src/Voucher/Domain/Entity/Voucher.php
+++ src/Voucher/Domain/Entity/Voucher.php
@@ -4,26 +4,12 @@ declare(strict_types=1);
 
 namespace App\Voucher\Domain\Entity;
 
-use App\Shared\Domain\Event\DomainEvent;
-use App\Shared\Domain\Event\DomainEvents;
+use App\Shared\Domain\Event\AbstractAggregateRoot;
 
-final class Voucher
+final class Voucher extends AbstractAggregateRoot
 {
-    private DomainEvents $events;
-
     private function __construct()
     {
-        $this->events = new DomainEvents();
-    }
-
-    /** @return DomainEvent[] */
-    public function pullDomainEvents(): array
-    {
-        return $this->events->pull();
-    }
-
-    private function record(DomainEvent $event): void
-    {
-        $this->events->record($event);
+        parent::__construct();
     }
 }
